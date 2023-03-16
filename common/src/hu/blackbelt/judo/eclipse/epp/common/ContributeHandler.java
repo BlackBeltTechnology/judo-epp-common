7e807e2204b501672962dc3d8f3511a260d685a1
gaborflorian <gabor.florian@blackbelt.hu>
2023-03-16 17:37:40 +0100

    Fix tabs and line ends

--- common/src/hu/blackbelt/judo/eclipse/epp/common/ContributeHandler.java
+++ common/src/hu/blackbelt/judo/eclipse/epp/common/ContributeHandler.java
@@ -16,13 +16,13 @@ package hu.blackbelt.judo.eclipse.epp.common;
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
- * 
+ *
  * This Source Code may also be made available under the following Secondary
  * Licenses when the conditions for such availability set forth in the Eclipse
  * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
  * with the GNU Classpath Exception which is
  * available at https://www.gnu.org/software/classpath/license.html.
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  * #L%
  */
@@ -43,24 +43,24 @@ import org.eclipse.ui.handlers.HandlerUtil;
 
 public class ContributeHandler extends AbstractHandler {
 
-	private static final String CONTRIBUTE_URL = "https://www.eclipse.org/contribute/";
+    private static final String CONTRIBUTE_URL = "https://www.eclipse.org/contribute/";
 
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IWorkbenchWindow activeWorkbenchWindow = HandlerUtil.getActiveWorkbenchWindow(event);
-		if (activeWorkbenchWindow == null) {
-			return new Status(IStatus.ERROR, "hu.blackbelt.judo.eclipse.epp.common", "No active workbench window");
-		}
-		try {
-			IWebBrowser browser = activeWorkbenchWindow.getWorkbench().getBrowserSupport().createBrowser(getClass().getName());
-			browser.openURL(new URL(CONTRIBUTE_URL));
-			return Status.OK_STATUS;
-		} catch (PartInitException e) {
-			Program.launch(CONTRIBUTE_URL);
-			return new Status(IStatus.OK, "hu.blackbelt.judo.eclipse.epp.common", e.getMessage(), e);
-		} catch (MalformedURLException e) {
-			return new Status(IStatus.ERROR, "hu.blackbelt.judo.eclipse.epp.common", e.getMessage(), e);
-		}
-	}
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        IWorkbenchWindow activeWorkbenchWindow = HandlerUtil.getActiveWorkbenchWindow(event);
+        if (activeWorkbenchWindow == null) {
+            return new Status(IStatus.ERROR, "hu.blackbelt.judo.eclipse.epp.common", "No active workbench window");
+        }
+        try {
+            IWebBrowser browser = activeWorkbenchWindow.getWorkbench().getBrowserSupport().createBrowser(getClass().getName());
+            browser.openURL(new URL(CONTRIBUTE_URL));
+            return Status.OK_STATUS;
+        } catch (PartInitException e) {
+            Program.launch(CONTRIBUTE_URL);
+            return new Status(IStatus.OK, "hu.blackbelt.judo.eclipse.epp.common", e.getMessage(), e);
+        } catch (MalformedURLException e) {
+            return new Status(IStatus.ERROR, "hu.blackbelt.judo.eclipse.epp.common", e.getMessage(), e);
+        }
+    }
 
 }
